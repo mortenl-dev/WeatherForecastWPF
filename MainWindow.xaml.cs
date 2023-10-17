640643ceb7d7efc0ae27ac9a9d3ba9c1cc53cda9
mortenl-dev <mortenlins@gmail.com>
2023-10-18 00:14:29 +0200

    Choose amount of days to forecast

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using OxyPlot;
 using OxyPlot.Series;
+using OxyPlot.Axes;
+using System.Windows.Media.Animation;
 
 
 namespace WeatherForecastWPF
@@ -34,7 +36,7 @@ namespace WeatherForecastWPF
             
             InitializeComponent();
             
-            OutputBox = element.CreateTextElement("Output");
+            OutputBox = element.CreateTextElement("Weather Forecast by mortenl-dev");
             MainPanel.Children.Add(OutputBox);
             
         }
@@ -43,34 +45,65 @@ namespace WeatherForecastWPF
 
         WeatherData? fetched;
         private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            string latitude = Latitude.Text;
-            string longitude = Longitude.Text;
-            fetched = fetch.CallApi($"https://api.open-meteo.com/v1/forecast?latitude={latitude}.52&longitude={longitude}.41&hourly=temperature_2m");
-            Longitude.Text = fetched.Timezone;
+        {   
+            CreateFetch();
+            
+        }
+
+        private void CreateFetch() {
+            try {
+                int templat = Int16.Parse(Latitude.Text);
+                int templong= Int16.Parse(Longitude.Text);
+                int tempdays = Int16.Parse(DaysCount.Text);
+                string latitude = templat.ToString();
+                string longitude = templong.ToString();
+                string days = tempdays.ToString();
+                fetched = fetch.CallApi($"https://api.open-meteo.com/v1/forecast?latitude={latitude}.52&longitude={longitude}.41&hourly=temperature_2m&forecast_days={days}");
+            }
+            catch (FormatException) {
+                return;
+            }
+            
+            
             CreateGraph();
         }
         private void CreateGraph()
         {
             // Create the PlotModel
-            PlotModel plotModel = new PlotModel { Title = "Sample Graph" };
+            PlotModel plotModel = new PlotModel { Title = "Weather forecast in degrees Celsius" };
 
+            // Create X and Y axes
+            var yAxis = new LinearAxis { Position = AxisPosition.Left, Title = "Celsius C" };
+
+            // Add axes to the plot model
+            
+            plotModel.Axes.Add(yAxis);
             // Create a LineSeries for the graph
+            
+
+            var xAxis = new DateTimeAxis
+            {
+                Position = AxisPosition.Bottom,
+                Title = "Time t",
+                
+                StringFormat = "yyyy-MM-dd HH:mm", // Format for displaying dates
+                MajorStep = 1,
+            };
+            plotModel.Axes.Add(xAxis);
             LineSeries series = new LineSeries
             {
                 Title = "Data Series",
                 StrokeThickness = 2,
-                MarkerType = MarkerType.Circle,
                 MarkerSize = 4
             };
-
             // Add data points (you can replace this with your data)
-            for (int x = 0; x < fetched.Hourly.Time.Count; x ++)
+            for (int x = 0; x < fetched!.Hourly!.Time!.Count; x ++)
             {   
-                float y = (float) Convert.ToDouble(fetched.Hourly.Temperature_2m[x]);
-                series.Points.Add(new DataPoint(x, y));
+                double time = DateTimeAxis.ToDouble(DateTime.Parse(fetched.Hourly.Time[x]));
+                float y = (float) Convert.ToDouble(fetched.Hourly.Temperature_2m![x]);
+                series.Points.Add(new DataPoint(time, y));
             }
-
+            
             // Add the series to the plot model
             plotModel.Series.Add(series);
 
